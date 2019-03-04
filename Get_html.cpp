569d9ecdca4df810e7bee1e59f161c248bb87b61
一只鱼笔 <843479000@qq.com>
2019-03-04 20:27:25 +0800

    complete Get_html.cpp

--- Get_html.cpp
+++ Get_html.cpp
@@ -1,55 +1,59 @@
 #include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<unistd.h>
 #include<sys/socket.h>
 #include<sys/types.h>
+#include<netinet/in.h>
 #define PORT 8848
 #define	BACKLOG 10
 using namespace std;
 
 int main(){
-	int sockfd=socket(AT_INET,SOCK_STREAM,0);//建立套接字
+	int sockfd=socket(AF_INET,SOCK_STREAM,0);//建立套接字
 	if(sockfd==-1){
 		perror("Socket");
 		exit(1);
 	}
-	struct sockaddr_in addr;
+	struct sockaddr_in addr;			//在头文件<netinet/in.h>中
 	addr.sin_family=AF_INET;
 	addr.sin_port=htons(PORT);			//转化为网络字节序
-	addr.sin_addr.s_addr=INADDR_ANY； 	//初始化为我的IP
-	bzero(&(my_addr.sin_zero));			//多余的字节初始为0
+	addr.sin_addr.s_addr=INADDR_ANY; 	//初始化为我的IP
+	bzero(&(addr.sin_zero),sizeof(addr.sin_zero));	//多余的字节初始为0
 	
 	//将本地端口和套接字绑定
-	if(bind(sock,(const struct sockaddr*)&address,sizeof(address))==-1){
+	if(bind(sockfd,(const struct sockaddr*)&addr,sizeof(addr))==-1){
 		perror("bind");
 		exit(1);
 	}
 
-	if(listen(sock,BACKLOG)==-1){	//第二个参数是等待队列的长度
+	if(listen(sockfd,BACKLOG)==-1){	//第二个参数是等待队列的长度
 		perror("listen");
 		exit(1);
 	}
 	printf("listening...\n");
 
-	struct sockadd_in naddr;
+	struct sockaddr_in naddr;
 	socklen_t len;
-	int nsockfd=accept(sock,(struct sockaddr*)&naddr,&len);
+	int nsockfd=accept(sockfd,(struct sockaddr*)&naddr,&len);	//呼叫地址
 	if(nsockfd==-1){
 		perror("accept");
 		exit(1);
 	}
 	printf("client connecting...\n");
 
-	FILE *fp=fopen("./html/index.html","rb");
+	FILE *fp=fopen("/html/index.html","rb");
 	char buf[1024];
 	memset(buf,0,sizeof(buf));
 	fread(buf,1024,1,fp);
-	printf("Content is %s\n",buf);
-	if(send(nsock,buf,strlen(buf),0)==-1){
+	printf("Content is \n%s",buf);
+	if(send(nsockfd,buf,strlen(buf),0)==-1){
 		perror("send");
 		close(nsockfd);
 		exit(1);
 	}
 	
-	close(nsockfd);				//关闭网络连接
-	close(sockfd)
+	close(nsockfd);				//关闭网络连接,在unistd.h中
+	close(sockfd);				//close用以关闭一般的文件描述符
 	return 0;
 }
