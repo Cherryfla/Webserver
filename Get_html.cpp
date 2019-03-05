cd38cfc101d81a42c8ae930a68fb6a0340de3506
一只鱼笔 <843479000@qq.com>
2019-03-05 22:45:43 +0800

    Multiple file access

--- Get_html.cpp
+++ Get_html.cpp
@@ -7,11 +7,13 @@
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<sys/stat.h>
+#include<sys/mman.h>
+#include<fcntl.h>
 #include"Get_html.h"
 using namespace std;
 
-bool is_get_http(char* buff){	//判断是否为GET请求
-	return !strncmp(buff,"GET",3);
+bool is_get_http(char* request){		//判断是否为GET请求
+	return !strncmp(request,"GET",3);
 }
 
 char* get_file_name(char* buff){	//从请求中得到文件名
@@ -21,45 +23,84 @@ char* get_file_name(char* buff){	//从请求中得到文件名
 	return fp;
 }
 int get_file_size(char* file_name){	//得到文件大小	
-    FILE *fp=fopen(file_name,"r");
-    if(!fp) 
-    	return -1;
-    fseek(fp,0L,SEEK_END);
-    int size=ftell(fp);
-    fclose(fp);
-    return size;
+	FILE *fp=fopen(file_name,"r");
+	if(!fp) 
+		return -1;
+	fseek(fp,0,SEEK_END);
+	int size=ftell(fp);
+	fclose(fp);
+	return size;
 }
-void deal_http(int sock,char* buff){
-	char* fp=get_file_name(buff);
+char* get_file_type(char* file_path){	//返回响应报文的文件类型
+	char* tmp=strrchr(file_path,'.');
+	static char file_type[256];
+	if(tmp==NULL)
+		strcpy(file_type,"text/plain");
+	else
+		tmp++;
+	if(!strcmp(tmp,"html")||!strcmp(tmp,"htm"))
+		strcpy(file_type,"text/html");
+	else if(!strcmp(tmp,"css"))
+		strcpy(file_type,"text/css");
+	else if(!strcmp(tmp,"gif"))
+		strcpy(file_type,"image/gif");
+	else if(!strcmp(tmp,"jpeg")||!strcmp(tmp,"jpg"))
+		strcpy(file_type,"image/jpeg");
+	else if(!strcmp(tmp,"png"))
+		strcpy(file_type,"image/png");
+	else
+		strcpy(file_type,"text/plain");
+	return file_type;
+}
+void deal_http(int sock,char* request){
+	char* fp=get_file_name(request);
 	printf("file path is :%s\n",fp);
 	
 	FILE* fd=fopen(fp,"rb");
 
 	if(fd==NULL){
-		char Error[]="Error:404\n";
+		char Error[]="HTTP/1.0 404 Not Found\
+		 Content-Type: text/plain 404 not found by Manio";
 		send(sock,Error,strlen(Error),0);
 	}
 	else{
 		int file_size=get_file_size(fp);
-		char tmp[BUFFSIZE];
-		memset(tmp,0,sizeof(tmp));
-		fread(tmp,BUFFSIZE,1,fd);
-		fclose(fd);		//关闭文件
+		int opened_file=open(fp,O_RDONLY);
+		void* memfile=mmap(0,file_size,PROT_READ,MAP_PRIVATE,opened_file,0);
+		//存储映射，防止因文件过大爆栈
+		close(opened_file);		//关闭文件
 		
 		printf("The file size is:%d\r\n",file_size);
-		printf("The context is \n%s\n",tmp);
-		if(~send(sock,tmp,strlen(tmp),0)){
-			printf("Send successfully\n");
-		}
-		else{
-			perror("send");
-		}
+		printf("The context is \r\n");
+		write(1,memfile,file_size);
+		printf("\r\n");
+		
+		char* file_type=get_file_type(fp);
+		char buff[BUFFSIZE],FlTp[256],ConLen[256];
+		memset(buff,0,sizeof(buff));
+		memset(ConLen,0,sizeof(ConLen));
+		memset(FlTp,0,sizeof(FlTp));
+
+		strcat(buff,"HTTP1.0 200 ok\r\n");		//响应报文的格式
+		sprintf(FlTp,"Content-Type: %scharset=UTF-8\r\n",file_type);
+		strcat(buff,FlTp);
+	//	strcat(buff,"Content-Type: text/html;charset=UTF-8\r\n");
+		sprintf(ConLen,"Content-Length: %d\r\n\r\n",file_size);
+		strcat(buff,ConLen);
+
+		send(sock,buff,strlen(buff),0);
+		send(sock,memfile,file_size,0);
+		printf("Send successfully\n");
+
+		munmap(memfile,file_size);
 	}
 }
 int main(int argc,char *argv[]){
 	struct sockaddr_in addr,naddr;			//在头文件<netinet/in.h>中
 	socklen_t len;
 	char buff[BUFFSIZE];
+	
+	memset(buff,0,sizeof(buff));
 	int sockfd=socket(AF_INET,SOCK_STREAM,0);//建立套接字
 	if(sockfd==-1){
 		perror("Socket");
@@ -91,7 +132,7 @@ int main(int argc,char *argv[]){
 	printf("client connected\n");
 
 	recv(nsockfd,buff,BUFFSIZE-1,0);		//recv将接收到的数据放到buff中
-	send(nsockfd,buff,BUFFSIZE-1,0);		//向客户端发送buff中的内容
+//	send(nsockfd,buff,BUFFSIZE-1,0);		//向客户端发送buff中的内容
 
 	printf("Recive message from client: %s\n",buff);
 
--- Get_html.h
+++ Get_html.h
@@ -1,11 +1,12 @@
 #ifndef __GET_HTML_H_
 #define __GET_HTML_H_
 
-#define PORT 8848
-#define BACKLOG 10
+#define PORT 8848		//端口号
+#define BACKLOG 10		//等待队列长度
 #define BUFFSIZE 1024
 
-bool Get_http(char* buff);
+bool is_get_http(char* request);
+char* get_file_type(char* file_path);
 char* get_file_name(char* buff);
 int get_file_size(char *file_name);
 void deal_http(int sock,char* buff);
